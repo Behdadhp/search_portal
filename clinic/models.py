8890d4cdb0dcf011101a76f292e0270b1ab268a4
behdadhp <behdad.hajipour95@gmail.com>
2022-03-13 23:13:09 +0100

    added Doctors model

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+genders = (
+    ("Male", "Herr"),
+    ("Female", "Frau"),
+    ("Diverse", "Diverse")
+    )
+
+titles = (
+    ("Dr", "Dr"),
+    ("-", "-")
+)
+
+specialist_groups = (
+    ("Hals-Nasen-Ohrenheilkunde", "Hals-Nasen-Ohrenheilkunde"),
+    ("Frauenheilkunde", "Frauenheilkunde"),
+    ("Orthopädie", "Orthopädie"),
+    ("Dermatologie", "Dermatologie"),
+)
+
+class Doctors(models.Model):
+    gender             = models.CharField(max_length=8,choices=genders)
+    title              = models.CharField(max_length=8,choices=titles)
+    first_name         = models.CharField(max_length=32,blank=False,null=False)
+    last_name          = models.CharField(max_length=32,blank=False,null=False)
+    specialist_group   = models.CharField(max_length=32, choices=specialist_groups)
+    mail               = models.EmailField(max_length=64,blank=False,null=False,default="mail@mail.com")
+
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)
