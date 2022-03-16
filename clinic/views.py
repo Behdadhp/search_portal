0c43023715215d01704d4b92d1e5dff2854e91a0
behdadhp <behdad.hajipour95@gmail.com>
2022-03-16 21:50:39 +0100

    updated views.py ClinicListView

--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from clinic import models
+from django.db.models import Q
 # Create your views here.
+
+
+
+class ClinicListView(generic.ListView, LoginRequiredMixin):
+    context_object_name = 'clinic_lists'
+    queryset = models.Details.objects.all()
