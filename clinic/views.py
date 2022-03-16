f8f019e4216e66eef1418892bffbecea2442ea03
behdadhp <behdad.hajipour95@gmail.com>
2022-03-16 21:51:05 +0100

    updated views.py SearchResultView

--- clinic/views.py
+++ clinic/views.py
@@ -10,3 +10,16 @@ from django.db.models import Q
 class ClinicListView(generic.ListView, LoginRequiredMixin):
     context_object_name = 'clinic_lists'
     queryset = models.Details.objects.all()
+
+class SearchResultView (generic.TemplateView ,LoginRequiredMixin):
+    template_name = 'clinic/search_result.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('s')
+        clinic_lists = models.Details.objects.filter(
+        Q(street=query) | Q(city=query) | Q(contract=query) | Q(specialist_group=query)
+        )
+        context["clinic_lists"] = clinic_lists
+
+        return context
