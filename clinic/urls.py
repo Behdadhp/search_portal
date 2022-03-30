31c2bf8ece6e26c7e18bfd7781e1aecc79a649e5
behdadhp <behdad.hajipour95@gmail.com>
2022-03-30 15:19:25 +0200

    updated urls.py with login_required

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from clinic import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'clinic'
 
 urlpatterns =[
-    path('',views.ClinicListView.as_view(),name='clinic'),
+    path('',login_required(views.ClinicListView.as_view()),name='clinic'),
     path('search',views.SearchResultView.as_view(),name='search'),
     path('details/<int:pk>',views.ClinicDetailsView.as_view(),name='details')
     ]
